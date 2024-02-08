0533c968cf4af7b09715507b4db062a41fb59652
Cameron Bateman <cameron.bateman@oracle.com>
2024-02-07 18:00:07 -0800

    Ping 2.

--- src/main/java/com/oracle/todoapp/mytodolist/HealthCheckController.java
+++ src/main/java/com/oracle/todoapp/mytodolist/HealthCheckController.java
@@ -15,6 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
     @GetMapping(value = "/")
     public String getHealthCheck() {
-        return "I am in Alameda!";
+        return "I am in Alameda2!";
     }
 }
